df25211d8e3f699036aae83c93fe8da0b54adfbb
JadeQiong <jieqiong@andrew.cmu.edu>
2023-01-05 23:59:06 -0800

    add nav main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import ReactDOM from 'react-dom';
 
 function Clock(props){
   return(
@@ -18,21 +21,9 @@ function tick(){
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        111  Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Main/>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Main(){
+    return (
+        <div>
+        <h1>k:LL</h1>
+        </div>
+    )
+}
+export default Main;
