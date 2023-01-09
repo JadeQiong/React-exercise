3384400398b8f26bc152fd7a416fbfa506a21a4d
JadeQiong <jieqiong@andrew.cmu.edu>
2023-01-08 22:57:58 -0800

    add small demo1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Navbar(){
     return (
         <nav>
-            <img src=".../public/logo512.png" className='nav--icon' />
+            <img src="logo192.png" className='nav--icon' />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="">React Course - Project 1</h4>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
