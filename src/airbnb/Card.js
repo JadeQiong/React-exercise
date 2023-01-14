e53352f73693d3e35515ea0e8cf8053b2fcedf5d
JadeQiong <jieqiong@andrew.cmu.edu>
2023-01-13 17:28:00 -0800

    airbnb: add cards

--- src/airbnb/Card.js
+++ src/airbnb/Card.js
@@ -1,17 +1,26 @@
 import React from "react";
 
-export default function Card(){
+export default function Card(props){
+    var cardText
+    if(props.openSpots==0){
+        cardText="SOLD OUT"
+    }
+    if(props.location=="Online"){
+        cardText="Online"
+    }
+
     return (
         <div className="card">
-            <img src="pic.jpg" className="card--image"></img>
+            {cardText && <div className="card--badge">{cardText}</div>}
+            <img src={props.img} className="card--image"></img>
             <div className="card--stats">
                 <img src="logo192.png" className="card--star"></img>
-                <span>512</span>
-                <span className="gray">(89)</span>
-                <span className="gray">USA</span>
+                <span> {props.rating} </span>
+                <span className="gray">&nbsp; {props.reviewCount}  • </span>
+                <span className="gray">&nbsp; {props.location}</span>
             </div>
-            <p>Life lessons with KZ</p>
-            <p> <span className="bold">136 / person</span></p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"> <span className="bold">{props.price} / person</span></p>
         </div>       
     )
 }
--- src/airbnbApp.js
+++ src/airbnbApp.js
@@ -4,13 +4,33 @@ import ReactDOM from 'react-dom';
 import Navbar from './airbnb/Navbar';
 import Hero from './airbnb/Hero';
 import Card from './airbnb/Card';
+import airbnbData from './airbnbData';
+
+const cards =airbnbData.map(
+  function(data){
+    return (
+    <Card 
+    key={data.id}
+    img={data.coverImg}
+    rating={data.stats.rating}
+    reviewCount={data.stats.reviewCount}
+    location={data.location}
+    title={data.title}
+    price={data.price}
+    openSpots={data.openSpots}
+    />
+  )}
+);
 
 function AirbnbApp(){
   return (
     <div className='container'>
     <Navbar/>
     <Hero/>
-    <Card/>
+    <section className='cards-list'>
+    {cards}
+    </section>
+
     </div>
   )
 }
